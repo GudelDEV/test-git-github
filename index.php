3d5e0beaf1afbf00a2a455a488037046d3d36ac3
GudelDEV <gudeldeveloper@gmail.com>
2024-01-02 09:30:25 -0800

    membuat file index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git</title>
+</head>
+
+<body>
+    <h1>Coba Git</h1>
+    <p>ini di buat dari git dan akan di kirimkan ke github di branch features</p>
+</body>
+
+</html>
